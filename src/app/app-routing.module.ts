ed81a0646bdf0c7c19670bad0467956a0ed09f03
Anist Mampuru - BCX <anistm@bcx.co.za>
2019-05-29 11:38:17 +0200

    added landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StructureComponent }   from './structure/structure.component';
+import { HomeComponent } from './home/home.component'; 
+
 
 const routes: Routes = [
   { path: 'structure', component: StructureComponent },
-  { path: '', redirectTo: '/structure', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component : HomeComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
