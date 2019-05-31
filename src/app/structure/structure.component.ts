308ac7745c14514ec7e98a42b8234cf62cb0ace1
Anist Mampuru - BCX <anistm@bcx.co.za>
2019-05-31 08:37:20 +0200

    Anist latest

--- src/app/structure/structure.component.ts
+++ src/app/structure/structure.component.ts
@@ -12,6 +12,12 @@ export class StructureComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  onResize() {
+	
+//line.attr('x1',x1).attr('y1',y1).attr('x2',x2).attr('y2',y2);
+  }
+
   cardprofile = {
   	 "department": "",
   	 "name": "",
